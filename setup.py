c89b1a88bfb3f644b4cdca9664e2c15fa117e3a2
tkspuk <aoyunlw@163.com>
2020-04-18 14:53:57 +0800

    modify the setup.py

--- setup.py
+++ setup.py
@@ -15,18 +15,18 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     author="Locus Li",
-    author_email="Locus Li",
-    description="Network Automation and Programmability Abstraction Layer with huawei Enterprise switch support,"
+    author_email="locus@byto.top",
+    description="Network Automation and Programmability Abstraction Layer with huawei Enterprise switch(VRP) support,"
                 " Most of these models are S5700 series、S6700 series，etc",
     classifiers=[
         'Topic :: Utilities',
          'Programming Language :: Python',
          'Programming Language :: Python :: 2',
          'Programming Language :: Python :: 3',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS',
+         'Operating System :: POSIX :: Linux',
+         'Operating System :: MacOS',
     ],
-    url="https://github.com/napalm-automation/napalm-skeleton",
+    url="https://github.com/tkspuk/napalm-vrp",
     include_package_data=True,
     install_requires=reqs,
 )
