d4d0c81ef615654a7fc6b18dec694448fb01ee45
Anton Gridnev <anton@gridnev.pro>
2022-10-05 12:44:56 +1100

    Fix exceptions

--- napalm_huawei_vrp/huawei_vrp.py
+++ napalm_huawei_vrp/huawei_vrp.py
@@ -42,7 +42,7 @@ from napalm.base.exceptions import (
 from .utils.utils import pretty_mac
 
 from netmiko import ConnectHandler
-from netmiko.ssh_exception import NetMikoTimeoutException
+from netmiko.exceptions import NetMikoTimeoutException
 
 # Easier to store these as constants
 HOUR_SECONDS = 3600
