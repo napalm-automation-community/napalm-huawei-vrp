a17e8881b5806e0057baa67eac6d3cc3b6db2a90
Michael Alvarez <michael_alvarez@claro.com.do>
2023-09-09 22:51:48 -0400

    - Mocked Data created for all commands. This is still in development. - requirements-dev.txt updated with napalm current libs - huawei_vrp.py updated with Black format. - test framework updated with "napalm" current format - getter get_interfaces added mtu to be consistent with napalm current implementation

--- test/unit/conftest.py
+++ test/unit/conftest.py
@@ -1,24 +1,27 @@
-"""Test fixtures."""
+"""Test fixtures.
+Doc: https://napalm.readthedocs.io/en/latest/development/testing_framework.html
+"""
 from builtins import super
 
 import pytest
 from napalm.base.test import conftest as parent_conftest
 
 from napalm.base.test.double import BaseTestDouble
+from napalm_huawei_vrp import huawei_vrp
 
-from napalm_skeleton import skeleton
 
-
-@pytest.fixture(scope='class')
+@pytest.fixture(scope="class")
 def set_device_parameters(request):
     """Set up the class."""
+
     def fin():
         request.cls.device.close()
+
     request.addfinalizer(fin)
 
-    request.cls.driver = skeleton.SkeletonDriver
-    request.cls.patched_driver = PatchedSkeletonDriver
-    request.cls.vendor = 'skeleton'
+    request.cls.driver = huawei_vrp.VRPDriver
+    request.cls.patched_driver = PatchedVRPDriver
+    request.cls.vendor = "huawei_vrp"
     parent_conftest.set_device_parameters(request)
 
 
@@ -27,31 +30,33 @@ def pytest_generate_tests(metafunc):
     parent_conftest.pytest_generate_tests(metafunc, __file__)
 
 
-class PatchedSkeletonDriver(skeleton.SkeletonDriver):
-    """Patched Skeleton Driver."""
+class PatchedVRPDriver(huawei_vrp.VRPDriver):
+    """Patched VRP Driver."""
 
     def __init__(self, hostname, username, password, timeout=60, optional_args=None):
-        """Patched Skeleton Driver constructor."""
         super().__init__(hostname, username, password, timeout, optional_args)
 
-        self.patched_attrs = ['device']
-        self.device = FakeSkeletonDevice()
+        self.patched_attrs = ["device"]
+        self.device = FakeVRPDevice()
+
+    def disconnect(self):
+        pass
+
+    def is_alive(self):
+        return {"is_alive": True}  # In testing everything works..
+
+    def open(self):
+        pass
 
 
-class FakeSkeletonDevice(BaseTestDouble):
-    """Skeleton device test double."""
+class FakeVRPDevice(BaseTestDouble):
+    """VRP device test double."""
 
-    def run_commands(self, command_list, encoding='json'):
-        """Fake run_commands."""
-        result = list()
+    def send_command(self, command, **kwargs):
+        filename = "{}.txt".format(self.sanitize_text(command))
+        full_path = self.find_file(filename)
+        result = self.read_txt_file(full_path)
+        return str(result)
 
-        for command in command_list:
-            filename = '{}.{}'.format(self.sanitize_text(command), encoding)
-            full_path = self.find_file(filename)
-
-            if encoding == 'json':
-                result.append(self.read_json_file(full_path))
-            else:
-                result.append({'output': self.read_txt_file(full_path)})
-
-        return result
+    def disconnect(self):
+        pass
